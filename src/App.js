969dc3cc51b64b41df380812cbce39073925414c
Antony-ant777 <antonychris2004@gmail.com>
2024-08-30 00:20:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BiSolidReport } from "react-icons/bi";
+import { FaBook, FaClinicMedical } from "react-icons/fa";
+import { HiMiniRectangleGroup } from "react-icons/hi2";
+import { IoBookSharp, IoFileTray, IoMailOpen, IoManSharp } from "react-icons/io5";
+import { MdAccountCircle, MdSupervisorAccount } from "react-icons/md";
+import { RiDiscountPercentFill } from "react-icons/ri";
 
-function App() {
+
+import royal from './royal.png';
+
+function Studentedutracking() {
+    const [one,setOne]=useState(0);
+    const [two,setTwo]=useState(0);
+    const [three,setThree]=useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='leftside'>
+            <div>
+            <img src={royal} className='logo' /> <br></br>
+            <MdSupervisorAccount  className='lefticon'/>
+            <li>Student Management</li>
+            <IoFileTray className='lefticon' />
+            <li>Financial Management</li>
+            <RiDiscountPercentFill className='lefticon'/>
+            <li>Quality Control</li>
+            <HiMiniRectangleGroup className='lefticon' />
+            <li>Report Delivery</li>
+
+            <BiSolidReport className='lefticon'/> 
+            <li>Attendance</li>
+            </div>
+             </div>
+
+             <div className='top'>
+                <p>Student Management</p>
+            </div>
+
+            <div className='lineone'>
+                <div className='box'>
+                    <div className='boxin' onClick={()=>setOne(one+1)}>
+                    <div >
+                    <MdAccountCircle className='icon' />
+                    </div>
+                    <div className='num'>
+                        <h3>{one}</h3>
+                    </div>
+                    </div>
+                    <p>Regular Student</p>
+                </div>
+                <div className='box'>
+                    <div className='boxin' onClick={()=>setTwo(two+1)}>
+                    <div >
+                    <MdAccountCircle className='icon' />
+                    </div>
+                    <div className='num'>
+                        <h3>{two}</h3>
+                    </div>
+                    </div>
+                    <p>Remaindal Student</p>
+                </div>
+                <div className='box'>
+                    <div className='boxin' onClick={()=>setThree(three+1)}>
+                    <div >
+                    <IoManSharp className='icon'/>
+                    </div>
+                    <div className='num'>
+                        <h3>{three}</h3>
+                    </div>
+                    </div>
+                    <p>IN Paid Clubs</p>
+                </div>
+                
+            </div>
+            <hr></hr>
+            <h1>Menu</h1>
+            <div className='line2'>
+                <div className='one'>
+                    <MdAccountCircle className='ic'/>
+                    <h4>Regular Enrollment</h4>
+                </div>
+                <div className='one'>
+                    <FaBook  className='ic'/>
+                    <h4>Remaindal Enrollment</h4>
+                </div>
+                <div className='one'>
+                    <IoManSharp className='ic'/>
+                    <h4>Club Management</h4>
+                </div>
+            </div>
+            <div className='line3'>
+                <div className='one'>
+                    <IoBookSharp  className='ic'/>
+                    <h4>Classroom Management</h4>
+                    
+                </div>
+                <div className='one'>
+                    <IoMailOpen  className='ic'/>
+                    <h4>SMS/EMAIL</h4>
+                </div>
+                <div className='one'>
+                    <BiSolidReport className='ic'/>
+                    <h4>Attendance</h4>
+                </div>
+
+            </div>
+            <div className='line4'>
+                <div className='one'>
+                    <FaClinicMedical  className='ic'/>
+                    <h4>Clinic</h4>
+                </div>
+                
+            </div>
+                
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default Studentedutracking
+
+
+
+
+
